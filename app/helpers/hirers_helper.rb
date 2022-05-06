46086805d8fda48d9cad69f7d8501c0873968ccd
Ubuntu <azureuser@railsvm.hiqpufyakhnehggv4ozcka3wmb.bx.internal.cloudapp.net>
2022-05-06 09:13:59 +0000

    fixed helper

--- app/helpers/hirers_helper.rb
+++ app/helpers/hirers_helper.rb
@@ -3,6 +3,11 @@ module HirersHelper
     one_near = 1000000000000000000000000
     second_per_hour = 3600
 
-    price_per_hour * one_near / second_per_hour
+    # Count the number of decimals
+    # https://stackoverflow.com/questions/8597766/how-to-count-the-number-of-decimal-places-in-a-float
+    decimal = price_per_hour.to_s.split('.').last.size 
+
+    price_int = (price_per_hour * decimal).to_i * one_near / decimal
+    price_int / second_per_hour
   end
 end
